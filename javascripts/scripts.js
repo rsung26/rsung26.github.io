a6641f2d9d904d19bcf9f2770e9e7aabb4c1a0b7
rsung26 <rsung26@gmail.com>
2014-03-19 17:54:44 -0400

    redesigned portfolio section and overall structure

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,14 +1,13 @@
 
 
-$(function(){
-		$(".project-item a").tooltip({ 
-			position: { my: "left+15 top-15", at: "right center" }, 
-			content: 'screenshot' }
-		);
+// $(function(){
+// 		$(".project-item a").tooltip({ 
+// 			position: { my: "left+15 top-15", at: "right center" }, 
+// 			content: 'screenshot' }
+// 		);
 
-		$("#lifelines").tooltip( "option", "content", '<img src="images/lifelines.png" width="300px" height="180px"></img>');
-		$("#html5-msc").tooltip( "option", "content", '<img src="images/html5-msc.png" width="300px" height="180px"></img>');
-		$("#balderdash").tooltip( "option", "content", '<img src="images/balderdash.png" width="300px" height="180px"></img>');
-		$("#ga-nlp").tooltip( "option", "content", '<img src="images/ga-nlp.png" width="300px" height="180px"></img>');
-});
-
+// 		$("#lifelines").tooltip( "option", "content", '<img src="images/lifelines.png" width="300px" height="180px"></img>');
+// 		$("#html5-msc").tooltip( "option", "content", '<img src="images/html5-msc.png" width="300px" height="180px"></img>');
+// 		$("#balderdash").tooltip( "option", "content", '<img src="images/balderdash.png" width="300px" height="180px"></img>');
+// 		$("#ga-nlp").tooltip( "option", "content", '<img src="images/ga-nlp.png" width="300px" height="180px"></img>');
+// });
